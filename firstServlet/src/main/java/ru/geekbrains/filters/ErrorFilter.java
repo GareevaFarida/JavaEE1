c57ea30d4b0392593c657247feec9801601f88a6
GareevaFarida <gareeva.farida@gmail.com>
2020-06-02 23:12:55 +0300

    ПР2

---
+++ firstServlet/src/main/java/ru/geekbrains/filters/ErrorFilter.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author Farida Gareeva
+ * Created 02.06.2020
+ */
+
+//не работает! Даже в случае неверного URL возвращается статус 200!
+@WebFilter(urlPatterns = "/*",dispatcherTypes = {DispatcherType.ERROR,DispatcherType.REQUEST})
+public class ErrorFilter implements Filter {
+
+    private static Logger logger = LoggerFactory.getLogger(ErrorFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        logger.info("Status code "+httpResponse.getStatus());
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
